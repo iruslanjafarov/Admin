b453b149fe801744f2e22386f874aac34a598584
Ruslan Jafarov <rusyajaf@gmail.com>
2024-06-07 15:21:38 +0300

    start point

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,16 @@
-import {defineConfig} from "vite"
-import react from "@vitejs/plugin-react"
-import autoprefixer from "autoprefixer";
-
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
-    base: "./",
+    base: './',
     build: {
         minify: true,
         sourcemap: true
     },
     server: {
-        host: "0.0.0.0",
-        port: "8888",
+        host: '0.0.0.0',
+        port: '8888',
         hmr: true
     },
     plugins: [react()],
