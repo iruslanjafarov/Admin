eb0b2fcdd6f98288728a50ce929398cd128f2a72
Ruslan Jafarov <rusyajaf@gmail.com>
2024-06-19 18:00:51 +0300

    fix filename

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './app/app'
+import App from './App/App'
 
 import '../styles/style.scss'
 
@@ -10,3 +10,4 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 root.render(
     <App/>
 )
+
